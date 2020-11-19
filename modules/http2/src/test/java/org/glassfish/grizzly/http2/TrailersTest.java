71506c2b5fde8896e2947b17882a7ae5e3754f37
David Matejcek <dmatej@seznam.cz>
2020-11-19 08:53:13 +0100

    FISH-464 Fixed TrailersTest
    - if the queue size is not same as reserved space, and if it is the last content
      and the content is a trailer, send trailers
    - if the queue size is same as reserved space, and record was already processed,
      don't send trailers.
    - continue otherwise.
    - TrailersTest - just formatting and finals

--- modules/http2/src/test/java/org/glassfish/grizzly/http2/TrailersTest.java
+++ modules/http2/src/test/java/org/glassfish/grizzly/http2/TrailersTest.java
@@ -36,6 +36,7 @@ import org.glassfish.grizzly.http.util.MimeHeaders;
 import org.glassfish.grizzly.memory.Buffers;
 import org.glassfish.grizzly.memory.MemoryManager;
 import org.glassfish.grizzly.nio.transport.TCPNIOConnectorHandler;
+import org.glassfish.grizzly.nio.transport.TCPNIOTransport;
 import org.junit.After;
 import org.junit.Test;
 
@@ -76,10 +77,10 @@ public class TrailersTest extends AbstractHttp2Test {
         configureHttpServer();
         startHttpServer(new HttpHandler() {
             @Override
-            public void service(Request request, Response response) throws Exception {
+            public void service(final Request request, final Response response) throws Exception {
                 response.setContentType("text/plain");
                 final InputStream in = request.getInputStream();
-                StringBuilder sb = new StringBuilder();
+                final StringBuilder sb = new StringBuilder();
                 int b;
                 while ((b = in.read()) != -1) {
                     sb.append((char) b);
@@ -104,7 +105,7 @@ public class TrailersTest extends AbstractHttp2Test {
 
         final Filter filter = new BaseFilter() {
             @Override
-            public NextAction handleRead(FilterChainContext ctx) throws IOException {
+            public NextAction handleRead(final FilterChainContext ctx) throws IOException {
                 final HttpContent httpContent = ctx.getMessage();
                 try {
                     if (lastProcessed.get()) {
@@ -121,7 +122,7 @@ public class TrailersTest extends AbstractHttp2Test {
                         latch.countDown();
                     } else {
                         assertFalse(httpContent instanceof HttpTrailer);
-                        int result = contentCount.incrementAndGet();
+                        final int result = contentCount.incrementAndGet();
                         if (result == 1) {
                             assertTrue(httpContent.getContent().remaining() == 0); // response
                         } else if (result == 2) {
@@ -130,7 +131,7 @@ public class TrailersTest extends AbstractHttp2Test {
                             fail("Unexpected content");
                         }
                     }
-                } catch (Throwable t) {
+                } catch (final Throwable t) {
                     error.set(t);
                     latch.countDown();
                 }
@@ -139,17 +140,11 @@ public class TrailersTest extends AbstractHttp2Test {
             }
         };
         final Connection<?> c = getConnection("localhost", PORT, filter);
-        HttpRequestPacket.Builder builder = HttpRequestPacket.builder();
-        HttpRequestPacket request = builder.method(Method.POST)
-                .uri("/echo")
-                .protocol(Protocol.HTTP_2_0)
-                .host("localhost:" + PORT).build();
-        c.write(HttpTrailer.builder(request)
-                .content(Buffers.wrap(MemoryManager.DEFAULT_MEMORY_MANAGER, "a=b&c=d"))
-                .last(true)
-                .header("trailer-a", "value-a")
-                .header("trailer-b", "value-b")
-                .build());
+        final HttpRequestPacket.Builder builder = HttpRequestPacket.builder();
+        final HttpRequestPacket request = builder.method(Method.POST).uri("/echo")
+            .protocol(Protocol.HTTP_2_0).host("localhost:" + PORT).build();
+        c.write(HttpTrailer.builder(request).content(Buffers.wrap(MemoryManager.DEFAULT_MEMORY_MANAGER, "a=b&c=d"))
+            .last(true).header("trailer-a", "value-a").header("trailer-b", "value-b").build());
         assertTrue(latch.await(10, TimeUnit.SECONDS));
         final Throwable t = error.get();
         if (t != null) {
@@ -164,7 +159,7 @@ public class TrailersTest extends AbstractHttp2Test {
         configureHttpServer();
         startHttpServer(new HttpHandler() {
             @Override
-            public void service(Request request, Response response) throws Exception {
+            public void service(final Request request, final Response response) throws Exception {
                 response.setContentType("text/plain");
                 final InputStream in = request.getInputStream();
                 //noinspection StatementWithEmptyBody
@@ -184,20 +179,19 @@ public class TrailersTest extends AbstractHttp2Test {
 
         final Filter filter = new BaseFilter() {
             @Override
-            public NextAction handleRead(FilterChainContext ctx) throws IOException {
+            public NextAction handleRead(final FilterChainContext ctx) throws IOException {
                 final HttpContent httpContent = ctx.getMessage();
                 try {
                     if (httpContent.isLast()) {
                         assertTrue(httpContent instanceof HttpTrailer);
                         final MimeHeaders trailers = ((HttpTrailer) httpContent).getHeaders();
-
                         assertEquals(2, trailers.size());
                         assertEquals("value-a", trailers.getHeader("trailer-a"));
                         assertEquals("value-b", trailers.getHeader("trailer-b"));
-                        latch.countDown();
                     }
-                } catch (Throwable t) {
+                } catch (final Throwable t) {
                     error.set(t);
+                } finally {
                     latch.countDown();
                 }
 
@@ -205,20 +199,14 @@ public class TrailersTest extends AbstractHttp2Test {
             }
         };
         final Connection<?> c = getConnection("localhost", PORT, filter);
-        HttpRequestPacket.Builder builder = HttpRequestPacket.builder();
-        HttpRequestPacket request = builder.method(Method.POST)
-                .uri("/echo")
-                .protocol(Protocol.HTTP_2_0)
-                .host("localhost:" + PORT).build();
-        c.write(HttpContent.builder(request) // write the request
-                .last(false)
-                .build());
-        c.write(HttpTrailer.builder(request) // write the trailer
-                .content(Buffers.EMPTY_BUFFER)
-                .last(true)
-                .header("trailer-a", "value-a")
-                .header("trailer-b", "value-b")
-                .build());
+        final HttpRequestPacket.Builder builder = HttpRequestPacket.builder();
+        final HttpRequestPacket request = builder.method(Method.POST).uri("/echo")
+            .protocol(Protocol.HTTP_2_0).host("localhost:" + PORT).build();
+        // write the request
+        c.write(HttpContent.builder(request).last(false).build());
+        // write the trailer
+        c.write(HttpTrailer.builder(request).content(Buffers.EMPTY_BUFFER).last(true)
+            .header("trailer-a", "value-a").header("trailer-b", "value-b").build());
         assertTrue(latch.await(10, TimeUnit.SECONDS));
         final Throwable t = error.get();
         if (t != null) {
@@ -249,12 +237,9 @@ public class TrailersTest extends AbstractHttp2Test {
         final FilterChain clientChain =
                 createClientFilterChainAsBuilder(false, true, clientFilter).build();
 
-        SocketConnectorHandler connectorHandler = TCPNIOConnectorHandler.builder(
-                httpServer.getListener("grizzly").getTransport())
-                .processor(clientChain)
-                .build();
-
-        Future<Connection> connectFuture = connectorHandler.connect(host, port);
+        final TCPNIOTransport transport = httpServer.getListener("grizzly").getTransport();
+        final SocketConnectorHandler connectorHandler = TCPNIOConnectorHandler.builder(transport).processor(clientChain).build();
+        final Future<Connection> connectFuture = connectorHandler.connect(host, port);
         return connectFuture.get(10, TimeUnit.SECONDS);
 
     }
